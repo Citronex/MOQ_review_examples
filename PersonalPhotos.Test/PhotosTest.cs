b205c592273674a810717ea6d322e4d9e910ee48
francisco.mejias <francisco.mejias@mindbodyonline.com>
2022-01-24 13:04:35 -0500

    test for the Upload method

---
+++ PersonalPhotos.Test/PhotosTest.cs
@@ -0,0 +1,43 @@
+using System.Text;
+using System.Threading.Tasks;
+using Core.Interfaces;
+using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Internal;
+using Microsoft.AspNetCore.Mvc;
+using Moq;
+using PersonalPhotos.Models;
+using Xunit;
+using PersonalPhotos.Controllers;
+using PersonalPhotos.Models;
+
+namespace PersonalPhotos.Test
+{
+    public class PhotosTest
+    {
+        [Fact]
+        public async Task Upload_GIvenFileName_ReturnsDisplayAction()
+        {
+            //Arrange
+            var session = Mock.Of<ISession>();
+            session.Set("User", Encoding.UTF8.GetBytes("a@b.com"));
+            var context = Mock.Of<HttpContext>(x => x.Session == session);
+            
+            var accesor = Mock.Of<IHttpContextAccessor>(x => x.HttpContext == context);
+            var fileStorage = Mock.Of<IFileStorage>();
+            var keyGen = Mock.Of<IKeyGenerator>();
+            var photoMetadata = Mock.Of<IPhotoMetaData>();
+            var controller = new PhotosController(keyGen, accesor, photoMetadata, fileStorage);
+
+            var fromFile = Mock.Of<IFormFile>();
+            var model = Mock.Of<PhotoUploadViewModel>(x => x.File == fromFile);
+
+            //Act
+            var result = await controller.Upload(model) as RedirectToActionResult;
+            //Assert
+            Assert.Equal("Display", result.ActionName, ignoreCase: true);
+        }
+        
+        //Write tests for the Display() method.
+        
+    }
+}
